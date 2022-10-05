39cd648f39da0511f86a4bea032c66044515a1d5
Viktor Ivarsson <vicke.i93@gmail.com>
2022-10-05 12:51:29 +0200

    setup dotfiles script

---
+++ setup-dotfiles.sh
@@ -0,0 +1,9 @@
+setup_link() {
+    ln -nfs "$HOME/code/dotfiles/$1" "$HOME/${2:-$1}"
+}
+
+setup_link ".config/cheat"
+setup_link ".config/nvim"
+setup_link ".tmux.conf"
+setup_link ".zshrc"
+setup_link "Brewfile"
