e556418bc82136b42c419dfc396b9ba58922aa64
Viktor Ivarsson <vicke.i93@gmail.com>
2022-12-03 23:55:08 +0100

    update gitsigns config

--- .config/nvim/lua/viktor/gitsigns.lua
+++ .config/nvim/lua/viktor/gitsigns.lua
@@ -37,20 +37,12 @@ gitsigns.setup({
 		end, { expr = true })
 
 		-- Actions
-		-- map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-		-- map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-		-- map('n', '<leader>hS', gs.stage_buffer)
-		-- map('n', '<leader>hu', gs.undo_stage_hunk)
-		-- map('n', '<leader>hR', gs.reset_buffer)
-		map("n", "<leader>hp", gs.preview_hunk)
+		map({ "n", "v" }, "<leader>rh", ":Gitsigns reset_hunk<CR>")
+		map("n", "<leader>ph", gs.preview_hunk)
 		map("n", "<leader>tb", gs.toggle_current_line_blame)
 		map("n", "<leader>hd", gs.diffthis)
 		map("n", "<leader>hD", function()
 			gs.diffthis("~")
 		end)
-		map("n", "<leader>td", gs.toggle_deleted)
-
-		-- Text object
-		map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
 	end,
 })
