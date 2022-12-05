1ee0b7f9300cc95e16208af02519336472e82190
Viktor Ivarsson <vicke.i93+github@gmail.com>
2022-12-05 09:29:35 +0100

    Revert "test lsp-zero"
    This reverts commit d3f2e6cce8e3c6649fd11e89b94ad3fcf3f1e21a.

--- .config/nvim/lua/viktor/packer.lua
+++ .config/nvim/lua/viktor/packer.lua
@@ -91,27 +91,20 @@ return require("packer").startup(function(use)
 		end,
 	})
 
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" },
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
-
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "hrsh7th/cmp-nvim-lua" },
-
-			-- Snippets
-			{ "L3MON4D3/LuaSnip" },
-			{ "rafamadriz/friendly-snippets" },
-		},
-	})
+	-- lsp
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-nvim-lsp-signature-help")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/nvim-cmp")
+	use({ "tzachar/cmp-tabnine", run = "./install.sh", requires = "hrsh7th/nvim-cmp" })
+	use("onsails/lspkind-nvim")
+	use("nvim-lua/lsp_extensions.nvim")
+	use("glepnir/lspsaga.nvim")
+	use("simrat39/symbols-outline.nvim")
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
 
 	-- format
 	use("sbdchd/neoformat")
