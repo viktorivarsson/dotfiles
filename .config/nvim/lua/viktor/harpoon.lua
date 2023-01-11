6e09ee0dcbcff9187b3a85a69f8be0103fc18de1
Viktor Ivarsson <vicke.i93+github@gmail.com>
2023-01-11 17:32:29 +0100

    update harpoon config

--- .config/nvim/lua/viktor/harpoon.lua
+++ .config/nvim/lua/viktor/harpoon.lua
@@ -1,12 +1,10 @@
 local map = vim.api.nvim_set_keymap
 
-vim.g.copilot_no_tab_map = true
-
-local options = { noremap = true, silent = true }
+local options = { noremap = true, siltent = true }
 
 map("n", "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<cr>", options)
-map("n", "<c-e>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", options)
-map("n", "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", options)
-map("n", "<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", options)
-map("n", "<leader>h3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", options)
-map("n", "<leader>h4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", options)
+map("n", "<leader>hl", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", options)
+map("n", "<leader>hq", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", options)
+map("n", "<leader>hw", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", options)
+map("n", "<leader>he", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", options)
+map("n", "<leader>hr", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", options)
