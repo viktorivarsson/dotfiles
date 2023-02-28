e1643765596d65fce7e8386aca723d3360b22224
Viktor Ivarsson <vicke.i93@gmail.com>
2023-02-28 10:02:54 +0100

    setup new theme config

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,8 +1,22 @@
 return {
-	-- "sam4llis/nvim-tundra",
-	"folke/tokyonight.nvim",
-
-	config = function()
-		vim.cmd("colorscheme tokyonight-night")
-	end,
+	{
+		"folke/tokyonight.nvim",
+		config = function()
+			-- vim.cmd([[colorscheme tokyonight-night]])
+		end,
+	},
+	{
+		"Yazeed1s/oh-lucy.nvim",
+		config = function()
+			-- vim.cmd([[colorscheme oh-lucy]]) -- for oh-lucy
+			-- vim.cmd([[colorscheme oh-lucy-evening]]) -- for oh-lucy-evening
+		end,
+	},
+	{
+		"olivercederborg/poimandres.nvim",
+		config = function()
+			require("poimandres").setup({})
+			vim.cmd([[colorscheme poimandres]])
+		end,
+	},
 }
