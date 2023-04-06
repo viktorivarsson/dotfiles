f381a931f4be33485f7499a5e6676bae498af9de
Viktor Ivarsson <vicke.i93+github@gmail.com>
2023-04-06 15:13:05 +0200

    update vim-fugitive bindings

--- .config/nvim/lua/plugins/vim-fugitive.lua
+++ .config/nvim/lua/plugins/vim-fugitive.lua
@@ -6,6 +6,32 @@ return {
 			local map = vim.api.nvim_set_keymap
 
 			map("n", "<leader>gs", "<cmd>Git<cr>", {})
+
+			local autocmd = vim.api.nvim_create_autocmd
+			autocmd("BufWinEnter", {
+				pattern = "*",
+				callback = function()
+					if vim.bo.ft ~= "fugitive" then
+						return
+					end
+
+					local bufnr = vim.api.nvim_get_current_buf()
+					local opts = { buffer = bufnr, remap = false }
+
+					vim.keymap.set("n", "<leader>gp", function()
+						vim.cmd.Git("push")
+					end, opts)
+
+					-- rebase always
+					vim.keymap.set("n", "<leader>gP", function()
+						vim.cmd.Git({ "pull", "--rebase" })
+					end, opts)
+
+					-- NOTE: It allows me to easily set the branch i am pushing and any tracking
+					-- needed if i did not set the branch up correctly
+					vim.keymap.set("n", "<leader>gt", ":Git push -u origin ", opts)
+				end,
+			})
 		end,
 	},
 }
