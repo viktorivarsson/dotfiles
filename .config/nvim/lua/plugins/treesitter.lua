5fed196ed239c9242c89bc059d4abbeedb7b746b
Viktor Ivarsson <vicke.i93+github@gmail.com>
2023-04-28 14:53:07 +0200

    Update treesitter.lua

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,9 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-context",
+		},
 		config = function()
 			require("nvim-treesitter.configs").setup({
 				auto_install = true,
