52515af89b1bb16d79b9b8fd45fe2256d9df7cba
Viktor Ivarsson <vicke.i93@gmail.com>
2023-05-02 13:24:33 +0200

    setup alacritty config

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -4,6 +4,7 @@ setup_link() {
 
 setup_link ".config/cheat"
 setup_link ".config/nvim"
+setup_link ".config/alacritty"
 setup_link ".config/brewfile/Brewfile"
 setup_link ".tmux.conf"
 setup_link ".zshrc"
