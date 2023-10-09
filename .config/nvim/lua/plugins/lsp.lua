ef2b35460a837593b7fb8f4657345c2f7224b70d
Viktor Ivarsson <vicke.i93+github@gmail.com>
2023-10-09 09:52:02 +0200

    add format ts errors

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -40,6 +40,7 @@ return {
 			{ "hrsh7th/cmp-nvim-lua" },
 			{ "hrsh7th/cmp-nvim-lsp-signature-help" },
 			{ "j-hui/fidget.nvim", tag = "legacy", opts = {} },
+			{ "davidosomething/format-ts-errors.nvim" },
 
 			-- completion
 			{
@@ -97,6 +98,32 @@ return {
 				tsserver = {
 					root_dir = nvim_lsp.util.root_pattern("package.json"),
 					single_file_support = false,
+					handlers = {
+						["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
+							if result.diagnostics == nil then
+								return
+							end
+
+							-- ignore some tsserver diagnostics
+							local idx = 1
+							while idx <= #result.diagnostics do
+								local entry = result.diagnostics[idx]
+
+								local formatter = require("format-ts-errors")[entry.code]
+								entry.message = formatter and formatter(entry.message) or entry.message
+
+								-- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+								if entry.code == 80001 then
+									-- { message = "File is a CommonJS module; it may be converted to an ES module.", }
+									table.remove(result.diagnostics, idx)
+								else
+									idx = idx + 1
+								end
+							end
+
+							vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+						end,
+					},
 				},
 				cssls = {},
 				denols = {
