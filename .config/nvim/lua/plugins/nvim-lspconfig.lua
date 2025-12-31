ac7570b1e520bce62458a6c0fdfb71e2f2cfc75f
Viktor Ivarsson <vicke.i93@gmail.com>
2025-12-31 01:02:11 +0100

    add tsgo to lspconfig

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,45 +1,45 @@
 local util = require("lspconfig.util")
 
 return {
-	"neovim/nvim-lspconfig",
-	opts = {
-		inlay_hints = {
-			enabled = false,
-		},
-		servers = {
-			svelte = {},
-			oxlint = {},
-			denols = {
-				root_dir = util.root_pattern("deno.json", "deno.jsonc"),
-			},
-			ts_ls = {
-				handlers = {
-					["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
-						if result.diagnostics == nil then
-							return
-						end
+  "neovim/nvim-lspconfig",
+  opts = {
+    inlay_hints = {
+      enabled = false,
+    },
+    servers = {
+      svelte = {},
+      oxlint = {},
+      denols = {
+        root_dir = util.root_pattern("deno.json", "deno.jsonc"),
+      },
+      tsgo = {
+        handlers = {
+          ["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
+            if result.diagnostics == nil then
+              return
+            end
 
-						-- ignore some tsserver diagnostics
-						local idx = 1
-						while idx <= #result.diagnostics do
-							local entry = result.diagnostics[idx]
+            -- ignore some diagnostics
+            local idx = 1
+            while idx <= #result.diagnostics do
+              local entry = result.diagnostics[idx]
 
-							local formatter = require("format-ts-errors")[entry.code]
-							entry.message = formatter and formatter(entry.message) or entry.message
+              local formatter = require("format-ts-errors")[entry.code]
+              entry.message = formatter and formatter(entry.message) or entry.message
 
-							-- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
-							if entry.code == 80001 then
-								-- { message = "File is a CommonJS module; it may be converted to an ES module.", }
-								table.remove(result.diagnostics, idx)
-							else
-								idx = idx + 1
-							end
-						end
+              -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+              if entry.code == 80001 then
+                -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
+                table.remove(result.diagnostics, idx)
+              else
+                idx = idx + 1
+              end
+            end
 
-						vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
-					end,
-				},
-			},
-		},
-	},
+            vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+          end,
+        },
+      },
+    },
+  },
 }
